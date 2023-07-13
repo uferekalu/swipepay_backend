02ccd4fb608601859c1557c23e03a93461dc5f2d
Ufere Goodnews <dekalusha@gmail.com>
2023-07-12 19:27:27 -0700

    modified

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ connectDB();
 app.use(express.json())
 app.use(cors())
 const corsOptions = {
-    "Access-Control-Allow-Origin": "http://localhost:4000"
+    "Access-Control-Allow-Origin": "https://swipepay-backend.vercel.app"
 }
 app.use(cors(corsOptions));
 
